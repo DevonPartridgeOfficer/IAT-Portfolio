73e6a4bb23378275e06a020cf1c7ddbe5ccb7801
Devon Partridge-Officer <90590089+DevonPartridgeOfficer@users.noreply.github.com>
2023-03-08 10:58:36 +1300

    Add enemy movement code (doesnt fully work)

--- Game_3_3D_Dungeon_Crawler/Assets/Scripts/GameController.cs
+++ Game_3_3D_Dungeon_Crawler/Assets/Scripts/GameController.cs
@@ -9,7 +9,8 @@ public class GameController : MonoBehaviour
     public GameObject monsterPrefab; //Add a monster with assigned prefab
 
     private MazeConstructor constructor;
-    
+    private AIController aIController;
+
     //To set the size of the maze
     [SerializeField] private int rows;
     [SerializeField] private int cols;
@@ -17,29 +18,37 @@ public class GameController : MonoBehaviour
     void Awake()
     {
         constructor = GetComponent<MazeConstructor>();
+        aIController = GetComponent<AIController>();
     }
 
     void Start()
     {
         constructor.GenerateNewMaze(rows, cols);
-        CreatePlayer();
-        CreateMonster();
+        aIController.Graph = constructor.graph;
+        aIController.Player = CreatePlayer();
+        aIController.Monster = CreateMonster();
+        aIController.HallWidth = constructor.hallWidth;
+        aIController.StartAI();
     }
 
     //Setup for player character
     //Player will starrt at the beginning of the maze [1,1]
-    private void CreatePlayer()
+    private GameObject CreatePlayer()
     {
         Vector3 playerStartPosition = new Vector3(constructor.hallWidth, 1, constructor.hallWidth);
         GameObject player = Instantiate(playerPrefab, playerStartPosition, Quaternion.identity);
         player.tag = "Generated";
+
+        return player;
     }
 
     //Creates the monster at the final cell of the world
-    private void CreateMonster()
+    private GameObject CreateMonster()
     {
         Vector3 monsterPosition = new Vector3(constructor.goalCol * constructor.hallWidth, 0f, constructor.goalRow * constructor.hallWidth);
         GameObject monster = Instantiate(monsterPrefab, monsterPosition, Quaternion.identity);
         monster.tag = "Generated";
+
+        return monster;
     }
 }
